4bcdd83ec89a827c01e46e2b6e5f0208124c8708
David Buschman <david.buschman@timeli.io>
2017-02-18 00:50:29 -0700

    added functional implementation

---
+++ scala/src/main/scala/com/gildedrose/GildedRoseFunctional.scala
@@ -0,0 +1,61 @@
+package com.gildedrose
+
+case class ImmutableItem(name: String, sellIn: Int, quality: Int)
+
+object GildedRoseFunctional {
+
+  // Rule : "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+  def updateQuality(items: Seq[ImmutableItem] = Seq.empty): Seq[ImmutableItem] = {
+
+    def rulesToApply(name: String): Seq[ImmutableItem ⇒ ImmutableItem] = name match {
+      case n if n.startsWith("Backstage passes") ⇒ Seq(decrementSellIn, ageBackstage, constrainQuality0to50)
+      case n if n.startsWith("Aged Brie")        ⇒ Seq(decrementSellIn, ageBrie, constrainQuality0to50)
+      case n if n.startsWith("Sulfuras")         ⇒ Seq(ageSulfuras) // do no decrement sellIn or constrain
+      case n if n.startsWith("Conjured")         ⇒ Seq(decrementSellIn, ageDownBy(2), constrainQuality0to50)
+      case _                                     ⇒ Seq(decrementSellIn, ageDownBy(1), constrainQuality0to50)
+    }
+
+    items.map { item ⇒
+      rulesToApply(item.name).foldLeft(item) { case (item, transform) ⇒ transform(item) }
+    }
+  }
+
+  // 
+  // Internal Rules for items
+  // ////////////////////////////////////
+
+  // Rule : At the end of each day our system lowers both values for every item [ NOTE : sellIn ONLY done here ] 
+  private[gildedrose] def decrementSellIn(in: ImmutableItem) = in.copy(sellIn = in.sellIn - 1)
+
+  // Rule : The Quality of an item is never negative
+  // Rule : The Quality of an item is never more than 50
+  private[gildedrose] def constrainQuality0to50(in: ImmutableItem) = in.copy(quality = Math.max(0, Math.min(in.quality, 50)))
+
+  // Rule : "Aged Brie" actually increases in Quality the older it gets
+  private[gildedrose] def ageBrie(item: ImmutableItem): ImmutableItem = item.copy(quality = item.quality + 1)
+
+  // Rule : Once the sell by date has passed, Quality degrades twice as fast
+  // Rule : At the end of each day our system lowers both values for every item [ NOTE : quality ONLY done here ] 
+  // Rule : "Conjured" items degrade in Quality twice as fast as normal items
+  private[gildedrose] def ageDownBy(ageBy: Int)(item: ImmutableItem): ImmutableItem = {
+    val newQuality = item.sellIn match {
+      case in if in > 0  ⇒ item.quality - ageBy
+      case in if in <= 0 ⇒ item.quality - (ageBy * 2)
+    }
+    item.copy(quality = newQuality)
+  }
+
+  // Rule : however "Sulfuras" is a legendary item and as such its Quality is 80 and it never alters.
+  private[gildedrose] def ageSulfuras(item: ImmutableItem): ImmutableItem = item.copy(quality = 80)
+
+  // Rule : "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+  //         Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+  //         Quality drops to 0 after the concert
+  private[gildedrose] def ageBackstage(item: ImmutableItem): ImmutableItem = item.sellIn match {
+    case in if in > 10 ⇒ item.copy(quality = item.quality + 1)
+    case in if in > 5  ⇒ item.copy(quality = item.quality + 3)
+    case in if in > 0  ⇒ item.copy(quality = item.quality + 5)
+    case _             ⇒ item.copy(quality = 0)
+  }
+
+}
--- scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -2,11 +2,93 @@ package com.gildedrose
 
 import org.scalatest._
 
-class GildedRoseTest  extends FlatSpec with Matchers {
-      it should "foo" in {
-        var items = Array[Item](new Item("foo", 0, 0))
-        val app = new GildedRose(items)
-        app.updateQuality()
-        (app.items(0).name) should equal ("fixme")
-      }
+class GildedRoseTest extends FunSuite with Matchers {
+
+  import GildedRoseFunctional._
+
+  test("decrementSellIn") {
+    decrementSellIn(ImmutableItem("name", 5, 1)) should be(ImmutableItem("name", 4, 1))
+    decrementSellIn(ImmutableItem("name", 1, 1)) should be(ImmutableItem("name", 0, 1))
+    decrementSellIn(ImmutableItem("name", -1, 1)) should be(ImmutableItem("name", -2, 1))
+  }
+
+  test("constrainAge0to50") {
+    constrainQuality0to50(ImmutableItem("name", 1, -1)) should be(ImmutableItem("name", 1, 0))
+    constrainQuality0to50(ImmutableItem("name", 1, 0)) should be(ImmutableItem("name", 1, 0))
+    constrainQuality0to50(ImmutableItem("name", 1, 1)) should be(ImmutableItem("name", 1, 1))
+    constrainQuality0to50(ImmutableItem("name", 1, 50)) should be(ImmutableItem("name", 1, 50))
+    constrainQuality0to50(ImmutableItem("name", 1, 51)) should be(ImmutableItem("name", 1, 50))
+  }
+
+  test("ageBrie") {
+    ageBrie(ImmutableItem("name", 1, -1)) should be(ImmutableItem("name", 1, 0))
+    ageBrie(ImmutableItem("name", 1, 1)) should be(ImmutableItem("name", 1, 2))
+  }
+
+  test("ageDownBy") {
+    // normal
+    ageDownBy(1)(ImmutableItem("name", 1, 10)) should be(ImmutableItem("name", 1, 9))
+    ageDownBy(2)(ImmutableItem("name", 1, 10)) should be(ImmutableItem("name", 1, 8))
+    ageDownBy(3)(ImmutableItem("name", 1, 10)) should be(ImmutableItem("name", 1, 7))
+
+    // double time
+    ageDownBy(1)(ImmutableItem("name", -1, 10)) should be(ImmutableItem("name", -1, 8))
+    ageDownBy(2)(ImmutableItem("name", -1, 10)) should be(ImmutableItem("name", -1, 6))
+    ageDownBy(3)(ImmutableItem("name", -1, 10)) should be(ImmutableItem("name", -1, 4))
+
+  }
+
+  test("ageSulfuras") {
+    ageSulfuras(ImmutableItem("name", 1, 1)) should be(ImmutableItem("name", 1, 80))
+    ageSulfuras(ImmutableItem("name", 1, 2)) should be(ImmutableItem("name", 1, 80))
+    ageSulfuras(ImmutableItem("name", 1, -1)) should be(ImmutableItem("name", 1, 80))
+  }
+
+  test("ageBackstage") {
+    ageBackstage(ImmutableItem("name", 50, 1)) should be(ImmutableItem("name", 50, 2))
+    ageBackstage(ImmutableItem("name", 11, 1)) should be(ImmutableItem("name", 11, 2))
+    ageBackstage(ImmutableItem("name", 10, 1)) should be(ImmutableItem("name", 10, 4))
+    ageBackstage(ImmutableItem("name", 6, 1)) should be(ImmutableItem("name", 6, 4))
+    ageBackstage(ImmutableItem("name", 5, 1)) should be(ImmutableItem("name", 5, 6))
+    ageBackstage(ImmutableItem("name", 1, 1)) should be(ImmutableItem("name", 1, 6))
+    ageBackstage(ImmutableItem("name", 0, 1)) should be(ImmutableItem("name", 0, 0))
+    ageBackstage(ImmutableItem("name", -1, 1)) should be(ImmutableItem("name", -1, 0))
+  }
+
+  test("updateQuality - aged brie") {
+    updateQuality(Seq(ImmutableItem("Aged Brie", 10, 10))).head should be(ImmutableItem("Aged Brie", 9, 11))
+    updateQuality(Seq(ImmutableItem("Aged Brie", 10, 50))).head should be(ImmutableItem("Aged Brie", 9, 50))
+  }
+
+  test("updateQuality - backstage passes") {
+    // level 1
+    updateQuality(Seq(ImmutableItem("Backstage passes to a TAFKAL80ETC concert", 20, 10))).head should be(ImmutableItem("Backstage passes to a TAFKAL80ETC concert", 19, 11))
+
+    // level 2 
+    updateQuality(Seq(ImmutableItem("Backstage passes to a TAFKAL80ETC concert", 8, 49))).head should be(ImmutableItem("Backstage passes to a TAFKAL80ETC concert", 7, 50))
+    updateQuality(Seq(ImmutableItem("Backstage passes to a TAFKAL80ETC concert", 8, 20))).head should be(ImmutableItem("Backstage passes to a TAFKAL80ETC concert", 7, 23))
+
+    // level 3
+    updateQuality(Seq(ImmutableItem("Backstage passes to a TAFKAL80ETC concert", 2, 20))).head should be(ImmutableItem("Backstage passes to a TAFKAL80ETC concert", 1, 25))
+
+    // level 4
+    updateQuality(Seq(ImmutableItem("Backstage passes to a TAFKAL80ETC concert", 0, 50))).head should be(ImmutableItem("Backstage passes to a TAFKAL80ETC concert", -1, 0))
+  }
+
+  test("updateQuality - sulfuras") {
+    updateQuality(Seq(ImmutableItem("Sulfuras, Hand of Ragnaros", 10, 10))).head should be(ImmutableItem("Sulfuras, Hand of Ragnaros", 10, 80))
+  }
+
+  test("updateQuality - commin items ") {
+    updateQuality(Seq(ImmutableItem("Some other item", 10, 65))).head should be(ImmutableItem("Some other item", 9, 50))
+    updateQuality(Seq(ImmutableItem("Some other item", 10, 10))).head should be(ImmutableItem("Some other item", 9, 9))
+    updateQuality(Seq(ImmutableItem("Some other item", -1, 10))).head should be(ImmutableItem("Some other item", -2, 8))
+  }
+
+  test("updateQuality - conjured") {
+    updateQuality(Seq(ImmutableItem("Conjured item", 10, 65))).head should be(ImmutableItem("Conjured item", 9, 50))
+    updateQuality(Seq(ImmutableItem("Conjured item", 10, 10))).head should be(ImmutableItem("Conjured item", 9, 8))
+    updateQuality(Seq(ImmutableItem("Conjured item", -1, 10))).head should be(ImmutableItem("Conjured item", -2, 6))
+  }
+
 }
